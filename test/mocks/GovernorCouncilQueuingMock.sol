48922a60d0b50b445d481d66e68cc654f3873cf9
funkyenough <14842981+funkyenough@users.noreply.github.com>
2025-11-05 15:48:47 +0900

    feat: add GovernorCouncilQueuingMock, and barebone test file

---
+++ test/mocks/GovernorCouncilQueuingMock.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Governor, IGovernor} from "@openzeppelin/contracts/governance/Governor.sol";
+import {GovernorSettings} from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import {GovernorCountingSimple} from
+  "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {
+  GovernorVotesQuorumFraction,
+  GovernorVotes
+} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {GovernorCouncilQueuing} from "src/extensions/GovernorCouncilQueuing.sol";
+
+contract GovernorCouncilQueuingMock is
+  GovernorCouncilQueuing,
+  GovernorSettings,
+  GovernorCountingSimple,
+  GovernorVotesQuorumFraction
+{
+  constructor(
+    string memory _name,
+    uint48 _initialVotingDelay,
+    uint32 _initialVotingPeriod,
+    uint256 _initialProposalThreshold,
+    IGovernor _vetoGovernor
+  )
+    Governor(_name)
+    GovernorCouncilQueuing(_vetoGovernor)
+    GovernorSettings(_initialVotingDelay, _initialVotingPeriod, _initialProposalThreshold)
+    GovernorVotesQuorumFraction(100)
+    GovernorVotes(IVotes(address(0)))
+  {}
+
+  function quorum(uint256 blockNumber)
+    public
+    view
+    override(Governor, GovernorVotesQuorumFraction)
+    returns (uint256)
+  {
+    return 100;
+  }
+
+  function state(uint256 proposalId)
+    public
+    view
+    override(Governor, GovernorCouncilQueuing)
+    returns (ProposalState)
+  {
+    return super.state(proposalId);
+  }
+
+  function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
+    return super.proposalThreshold();
+  }
+
+  function propose(
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    string memory description
+  ) public override(Governor, GovernorCouncilQueuing) returns (uint256) {
+    return super.propose(targets, values, calldatas, description);
+  }
+
+  function proposalNeedsQueuing(uint256 proposalId)
+    public
+    view
+    virtual
+    override(Governor, GovernorCouncilQueuing)
+    returns (bool)
+  {
+    return super.proposalNeedsQueuing(proposalId);
+  }
+
+  function _queueOperations(
+    uint256 proposalId,
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) internal override(Governor, GovernorCouncilQueuing) returns (uint48) {
+    return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
+  }
+
+  function exposed_queueOperations(
+    uint256 proposalId,
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) public {
+    _queueOperations(proposalId, targets, values, calldatas, descriptionHash);
+  }
+
+  function exposed_executeOperations(
+    uint256 proposalId,
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) public {
+    _executeOperations(proposalId, targets, values, calldatas, descriptionHash);
+  }
+
+  function exposed_cancel(
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) public {
+    _cancel(targets, values, calldatas, descriptionHash);
+  }
+
+  function exposed_checkVetoGovernorStateBitmap(uint256 proposalId, bytes32 allowedStates) public view {
+    _checkVetoGovernorStateBitmap(proposalId, allowedStates);
+  }
+
+  function _executeOperations(
+    uint256 proposalId,
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) internal override(Governor, GovernorCouncilQueuing) {
+    super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
+  }
+
+  function _cancel(
+    address[] memory targets,
+    uint256[] memory values,
+    bytes[] memory calldatas,
+    bytes32 descriptionHash
+  ) internal override(Governor, GovernorCouncilQueuing) returns (uint256) {
+    return super._cancel(targets, values, calldatas, descriptionHash);
+  }
+
+  function _executor() internal view override(Governor, GovernorCouncilQueuing) returns (address) {
+    return super._executor();
+  }
+}
