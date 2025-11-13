fee6d5404b832749f0fb45a5859029409706f54d
funkyenough <14842981+funkyenough@users.noreply.github.com>
2025-11-13 20:47:57 +0900

    docs: update natspec

--- src/extensions/GovernorAdmin.sol
+++ src/extensions/GovernorAdmin.sol
@@ -9,7 +9,7 @@ import {Ownable} from "@openzeppelin/contracts/Access/Ownable.sol";
  * @notice Extension of {Governor} that restricts privileged governance actions to a desginated
  * admin.
  * @dev Overrides `Governor-_checkGovernance` so that only the admin can perform
- * governance-restricted operations. This allows an external main DAO to control critical governance
+ * governance-restricted operations. This allows an external address to control critical governance
  * parameters while preventing the council from manipulating these settings.
  */
 abstract contract GovernorAdmin is Governor, Ownable {
